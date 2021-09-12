d7a994ba417b993dc70538368179ac95ed9d40c5
Ant <ryu4979@gmail.com>
2021-09-13 00:02:01 +0900

    refactor: DB面のリファクタリング

---
+++ models/centralLeague.js
@@ -0,0 +1,37 @@
+module.exports = {
+  async updateAll(client, teams) {
+    await client.query("BEGIN");
+    await client.query("TRUNCATE TABLE central_league RESTART IDENTITY");
+    teams.forEach(team => {
+      this.insert(client, team);
+    });
+    await client.query("COMMIT");
+  },
+
+  async insert(client, team) {
+    await client.query(`INSERT INTO central_league (
+      rank, name, play_game_count, win, lose,
+      draw, pct, game_diff, remaining_games, run,
+      ra, hr, sb, avg, era,
+      pythagorean_expectation
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8 ,$9, $10, $11, $12, $13, $14, $15, $16)`, [
+      team.rank,
+      team.name,
+      team.playGameCount,
+      team.win,
+      team.lose,
+      team.draw,
+      team.pct,
+      team.gameDiff,
+      team.remainingGames,
+      team.run,
+      team.ra,
+      team.hr,
+      team.sb,
+      team.avg,
+      team.era,
+      team.pythagoreanExpectation,
+    ]);
+    console.log("success!");
+  }
+};
--- update.js
+++ update.js
@@ -1,44 +1,11 @@
-const db = require("./db/db");
+const db = require("./db");
+const clModel = require("./models/centralLeague");
 const npb = require("./web-scraping/npb");
-
-async function centralLeagueInsert(client, team) {
-  await client.query(`INSERT INTO central_league (
-    rank, name, play_game_count, win, lose,
-    draw, pct, game_diff, remaining_games, run,
-    ra, hr, sb, avg, era,
-    pythagorean_expectation
-  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8 ,$9, $10, $11, $12, $13, $14, $15, $16)`, [
-    team.rank,
-    team.name,
-    team.playGameCount,
-    team.win,
-    team.lose,
-    team.draw,
-    team.pct,
-    team.gameDiff,
-    team.remainingGames,
-    team.run,
-    team.ra,
-    team.hr,
-    team.sb,
-    team.avg,
-    team.era,
-    team.pythagoreanExpectation,
-  ]);
-  console.log("success!");
-}
 
 async function main() {
   try {
     const client = await db.pool.connect();
-
-    await client.query("BEGIN");
-    await client.query("TRUNCATE TABLE central_league RESTART IDENTITY");
-    npb.standings("CL").forEach(team => {
-      centralLeagueInsert(client, team);
-    });
-    await client.query("COMMIT");
-
+    clModel.updateAll(client, npb.standings("CL"));
     await client.release();
   } catch (error) {
     console.error(error);
