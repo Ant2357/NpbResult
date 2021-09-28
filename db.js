4e846ee8e9dcbc67964359cf25e6dfcaf9fa7443
Ant <ryu4979@gmail.com>
2021-09-28 23:00:55 +0900

    fix: clientConfigの変更

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 exports.clientConfig = {
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.ENV_HOST,
+  database: process.env.ENV_DB,
+  user: process.env.ENV_USER,
+  port: 5432,
+  password: process.env.ENV_PASSWORD,
   ssl: { rejectUnauthorized: false }
 };
